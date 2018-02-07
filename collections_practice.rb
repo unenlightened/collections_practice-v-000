6cb5115bf4a58714fe16ac1bd9d2dd19702bf95c
Chely Ho <adept_inept@yahoo.com>
2018-02-07 22:41:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,9 +39,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index { |word, i|
-
-      word << "s" if i != 1
-
-  }
+  array.each_with_index {|word, i| word << "s" if i != 1}
 end
