c14aec7b1b31e42997d2785886bcefa7c63c912c
Chely Ho <adept_inept@yahoo.com>
2018-02-07 22:14:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,8 @@ def swap_elements(array)
   array[1], array[2] = array[2], array [1]
   array
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
